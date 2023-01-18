13b0113818cc77c1f8ef6295f81466e463feba3d
ilya <ilya.zakora@yandex.ru>
2023-01-18 22:30:34 +0300

    feat: add_modal_and_footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasketComponent } from './components/basket/basket.component';
+import { MainComponent } from './components/main/main.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'products', component: ProductComponent},
+  {path: 'product/id', component: ProductDetailComponent},
+  {path: 'basket', component: BasketComponent},
+
+  {path: '**', redirectTo: "", component: MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/UI/header/header.component';
+import { FooterComponent } from './components/UI/footer/footer.component';
+import { ProductComponent } from './components/product/product.component';
+import { BasketComponent } from './components/basket/basket.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { MainComponent } from './components/main/main.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductComponent,
+    BasketComponent,
+    ProductDetailComponent,
+    ModalComponent,
+    MainComponent
   ],
   imports: [
+    MatButtonModule,
+    MatToolbarModule,
     BrowserModule,
-    AppRoutingModule
+    MatCardModule,
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
